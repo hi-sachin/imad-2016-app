425456e8da16381aea87d8975471804d271dc7b8
hi-sachin <sacheenshelke@gmail.com>
2016-10-01 23:57:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,7 @@
-console.log('Loaded!');
-
-//Change the html body
-var element = document.getElementById('main-text');
-element.innerHTML='new Value';
-
-//Move the image
-var img=document.getElementById('sac');
-var margineLeft=0;
-function moveRight(){
-    margineLeft=margineLeft+1;
-    img.style.marginLeft=margineLeft+'px';
-}
-
-img.onclick = function() {
-    var interval = setInterval(moveRight,50);
+var button=document.getElementById('counter');
+var counter=0;
+button.onclick = function(){
+    counter=counter+1;
+    var span= document.getElementById('count');
+    span.innerHTML=counter.toString();
 };
