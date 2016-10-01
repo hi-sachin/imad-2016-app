da89a1e48535f030bb531c1a8bbb1b81c91da0af
hi-sachin <sacheenshelke@gmail.com>
2016-10-01 22:11:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 //Change the html body
 var element = document.getElementById("main-text");
 element.innerHTML ="new Value";
+
+//Move the image
+var img=document.getElementById("sac");
+img.onclick = function() {
+  img.style.marginleft='60px'  ;
+};
