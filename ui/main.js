bf40bd99e9beb7748d32a15decfae5a7099e90ea
hi-sachin <sacheenshelke@gmail.com>
2016-10-01 22:48:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,7 @@ element.innerHTML = 'new Value';
 var img=document.getElementById('sac');
 var margineLeft=0;
 function moveRight(){
-    margineLeft=margineLeft+10;
+    margineLeft=margineLeft+5;
     img.style.marginLeft=margineLeft+'px';
 }
 
